daca89da1ad2646e2f3ed070658e9054ff361c8a
Ayan <62609038+link1-n@users.noreply.github.com>
2020-05-26 19:45:48 +0530

    Date
    Added dates to the X-Axis of the graph

--- covid.py
+++ covid.py
@@ -7,6 +7,11 @@ url = requests.get(link)
 
 soup = BeautifulSoup(url.content, 'lxml')
 numbers = soup.findAll(class_='maincounter-number')
+date = soup.findAll(class_ = 'btn btn-light date-btn')
+
+dateS = []
+for i in reversed(range(6)):
+    dateS.append(date[i].getText())
 
 total = numbers[0].span.getText()
 total_int = int(total.replace(',',''))
@@ -20,28 +25,23 @@ j = 7
 newCase = soup.findAll(class_ = 'news_li', limit = j)
 
 newCaseT = []
-x = []
 
 for i in reversed(range(j)):
     lol = int(newCase[i].strong.getText().replace(' new cases','').replace(',',''))
     newCaseT.append(lol)
 
-for i in range(1, j+1):
-    x.append(i)
-
-x.pop(6)
 dailyCase = newCaseT[:] 
 #[:] was added because python creates a reference to the variable when simply equated
 dailyCase.pop(6)
-
-plt.plot(x, dailyCase, marker = 'o', markerfacecolor = 'pink')
-plt.xlabel("Day")
-plt.ylabel("Number of New Cases")
-plt.title("Number of New COVID-19 Cases in the Past 6 Days")
-plt.grid()
-plt.show()
 
 active = total_int - (death_int + recover_int)
 
 print(f"In India:\n\tTotal Cases: {total}\n\tRecovered: {recover}\n\tDeaths: {death}\n\tActive Cases: {active}")
 print(f"\tNumber of New Cases Today: {newCaseT[6]}\n\tNumber of New Cases Yesterday: {newCaseT[5]}")
+
+plt.plot(dateS, dailyCase, marker = 'o', markerfacecolor = 'pink')
+plt.xlabel("Day")
+plt.ylabel("Number of New Cases")
+plt.title("Number of New COVID-19 Cases in the Past 6 Days")
+plt.grid()
+plt.show()
